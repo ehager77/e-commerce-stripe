3e883ed4b3b1e53bfd74dd5efe9eb02d4ea64011
ehager77 <eric@isabel.io>
2019-07-19 16:16:39 -0700

    added first reducer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignUp from './pages/sign-up/sign-up.component';
-import SignIn from './pages/sign-in/sign-in.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 class App extends React.Component {
@@ -58,7 +57,6 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signup' component={SignUp} />
           <Route path='/signin' component={SignUp} />
         </Switch>
 
