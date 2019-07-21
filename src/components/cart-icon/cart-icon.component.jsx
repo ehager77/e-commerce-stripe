6b5e33490a7c8eb142cc572beddb03a9c663e256
ehager77 <eric@isabel.io>
2019-07-21 12:24:59 -0700

    added cart-icon incrementer

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,21 +2,26 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
-
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag-white.svg'
 
 import './cart-icon.styles.scss'
-import { dispatch } from 'rxjs/internal/observable/pairs';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
         <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon/>
-        <span style={{color: 'white'}} className='item-count'>{0}</span>
+        <span style={{color: 'white'}} className='item-count'>{itemCount}</span>
         </div>
 );
+
+const mapStateToProps = ({cart: {cartItems}}) =>({
+    itemCount: 
+    cartItems.reduce((totalQuantity, cartItem) => totalQuantity + cartItem.quantity, 
+    0
+    )
+});
 
 const mapDispatchToProps = dispatch =>({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
