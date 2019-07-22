5b3788c99d3a86c46505fb38016842d6fff1a6fa
ehager77 <eric@isabel.io>
2019-07-21 21:09:21 -0700

    added stripe checkout button component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,7 @@ import { createStructuredSelector } from 'reselect';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component'
 
 import './checkout.styles.scss';
 
@@ -32,6 +33,7 @@ const CheckoutPage = ({ cartItems, total }) => (
                         key={CheckoutItem.id} cartItem={cartItem} />
                 ))}
         <div className='cart-total'>Total: ${total}</div>
+        <StripeCheckoutButton price={total}/>
     </div>
 );
 
